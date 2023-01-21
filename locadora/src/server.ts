324d55f16f3ceaf8de2efaf8ca49ec3953bf9de0
Elan Fraga <elanfraga@gmail.com>
2023-01-21 16:56:36 -0300

    Criando categoria

--- locadora/src/server.ts
+++ locadora/src/server.ts
@@ -1,20 +1,11 @@
-import express, { response } from "express";
+import express from 'express';
+
+import { categoriesRoutes } from './routes/categories.routes';
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (request, response) => {
+app.use(categoriesRoutes);
 
-  return response.json({ message: "hello word" });
-});
-
-app.post("/course", (request, response) => {
-  const { name } = request.body;
-
-  return response.json({ name: name });
-  
-});
-
-
-app.listen(3333, () => console.log("Server is running"));
+app.listen(3333, () => console.log('Server is running'));
