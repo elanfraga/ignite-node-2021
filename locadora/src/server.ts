6cf3d2c372217a5359316be20a48d1e0ce527680
Elan Fraga <elanfraga@gmail.com>
2023-01-21 16:08:13 -0300

    Debugando a aplicação

--- locadora/src/server.ts
+++ locadora/src/server.ts
@@ -1,9 +1,20 @@
-import express from "express";
+import express, { response } from "express";
 
 const app = express();
 
-app.get("/", (req: express.Request, res: express) => {
-  return res.json({ message: "hello word" });
+app.use(express.json());
+
+app.get("/", (request, response) => {
+
+  return response.json({ message: "hello word" });
 });
+
+app.post("/course", (request, response) => {
+  const { name } = request.body;
+
+  return response.json({ name: name });
+  
+});
+
 
 app.listen(3333, () => console.log("Server is running"));
