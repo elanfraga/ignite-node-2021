c25b100951e801ef170ba6a6f61ce4c45144db94
Elan Fraga <elanfraga@gmail.com>
2023-01-28 00:17:42 -0300

    Conhecendo TSyringe

--- locadora/src/routes/categories.routes.ts
+++ locadora/src/routes/categories.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import multer from 'multer';
 
-import createCategoryController from '../modules/cars/useCases/createCategory';
+import { CreateCategoryController } from '../modules/cars/useCases/createCategory/CreateCategoryController';
 import { importCategoryController } from '../modules/cars/useCases/importCategory';
 import { listCategoriesController } from '../modules/cars/useCases/listCategories';
 
@@ -12,7 +12,9 @@ const upload = multer({
 
 });
 
-categoriesRoutes.post('/', (request, response) => createCategoryController().handle(request, response));
+const createCategoryController = new CreateCategoryController();
+
+categoriesRoutes.post('/', createCategoryController.handle);
 
 categoriesRoutes.get('/', (request, response) => listCategoriesController.handle(request, response));
 
