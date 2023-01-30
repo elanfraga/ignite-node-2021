38ab2e1d964f96a6503b6f2e064a3cee2e459332
Elan Fraga <elanfraga@gmail.com>
2023-01-30 20:00:50 -0300

    Tratamento de exceções

--- locadora/src/middlewares/ensureAuthenticated.ts
+++ locadora/src/middlewares/ensureAuthenticated.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 import { verify } from 'jsonwebtoken';
 
+import { AppError } from '../erros/AppError';
 import { UsersRepository } from '../modules/account/repositories/implementations/UsersRepository';
 
 interface IPayload {
@@ -10,7 +11,7 @@ export function ensureAuthenticated(request: Request, response: Response, next: 
   const authHeader = request.headers.authorization;
 
   if (!authHeader) {
-    throw new Error('Token missing');
+    throw new AppError('Token missing', 401);
   }
 
   const [, token] = authHeader.split(' ');
@@ -21,11 +22,11 @@ export function ensureAuthenticated(request: Request, response: Response, next: 
     const userRepository = new UsersRepository();
     const user = userRepository.findById(user_id);
     if (!user) {
-      throw new Error('User does not exist');
+      throw new AppError('User does not exist', 401);
     }
 
     next();
   } catch (error) {
-    throw new Error('Invalid token');
+    throw new AppError('Invalid token', 401);
   }
 }
