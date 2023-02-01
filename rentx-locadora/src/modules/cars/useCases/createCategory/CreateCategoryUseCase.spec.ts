db1548fb997dc8c1b878afd363caac99846500eb
Elan Fraga <elanfraga@gmail.com>
2023-02-01 07:25:23 -0300

    Criando o primeiro teste

---
+++ rentx-locadora/src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,15 @@
+describe('Criar categoria', () => {
+  it('Espero que 2 + 2 = 4', () => {
+    const soma = 2 + 2;
+    const resutado = 4;
+
+    expect(soma).toBe(resutado);
+  });
+
+  it('Espero que 2 + 2 não seja 5', () => {
+    const soma = 2 + 2;
+    const resutado = 5;
+
+    expect(soma).not.toBe(resutado);
+  });
+});
