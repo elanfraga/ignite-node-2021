bc88d8582cb9541d05a6aeae143413d3d73934f4
Elan Fraga <elanfraga@gmail.com>
2023-02-09 21:24:56 -0300

    Continuação da listagem de carros disponíveis

--- rentx-locadora/src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
+++ rentx-locadora/src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
@@ -39,9 +39,9 @@ class CarsRepositoryInMemory implements ICarsRepository {
   async findAvailable(brand?: string, category_id?: string, name?: string): Promise<Car[]> {
     const cars = this.cars
       .filter((car) => {
-        if (car.available === true && ((brand && car.brand === brand)
+        if (car.available === true || (brand && car.brand === brand)
         || (category_id && car.category_id === category_id)
-        || (name && car.name === name))) {
+        || (name && car.name === name)) {
           return car;
         }
         return null;
