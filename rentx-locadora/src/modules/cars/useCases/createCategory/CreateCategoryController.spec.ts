3725e9a391c5273f375117ce3fa34b46377020a3
Elan Fraga <elanfraga@gmail.com>
2023-02-24 21:02:20 -0300

    Criando o primeiro teste de integração

--- rentx-locadora/src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
+++ rentx-locadora/src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -3,9 +3,14 @@ import request from 'supertest';
 import { app } from '@shared/infra/http/app';
 
 describe('Create Category Controller', () => {
-  it('test', async () => {
-    await request(app)
-      .get('cars/available')
-      .expect(200);
+  it('should be able to create a category', async () => {
+    const response = await request(app)
+      .post('categories')
+      .send({
+        name: 'Category test name',
+        description: 'Test description',
+      });
+
+    expect(response.status).toBe(201);
   });
 });
