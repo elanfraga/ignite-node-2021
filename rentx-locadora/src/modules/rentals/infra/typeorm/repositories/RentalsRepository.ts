904d4aa7e2c4fa8bb07ee0424be1c4b59bbcd841
Elan Fraga <elanfraga@gmail.com>
2023-03-04 10:46:36 -0300

    Listagem de aluguéis do usuário

--- rentx-locadora/src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
+++ rentx-locadora/src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
@@ -61,6 +61,14 @@ class RentalsRepository implements IRentalsRepository {
 
     return rental;
   }
+
+  async findByUserId(user_id: string): Promise<Rental[]> {
+    const rentals = this.repository.find({
+      user_id,
+    });
+
+    return rentals;
+  }
 }
 
 export { RentalsRepository };
