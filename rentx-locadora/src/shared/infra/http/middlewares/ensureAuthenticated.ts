5319d4a2518e72e3220d9d1f8038c3c8c177c89e
Elan Fraga <elanfraga@gmail.com>
2023-03-07 23:30:24 -0300

    Controller refresh token

--- rentx-locadora/src/shared/infra/http/middlewares/ensureAuthenticated.ts
+++ rentx-locadora/src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -1,7 +1,9 @@
 import { NextFunction, Request, Response } from 'express';
 import { verify } from 'jsonwebtoken';
 
+import auth from '@config/auth';
 import { UsersRepository } from '@modules/account/infra/typeorm/repositories/UsersRepository';
+import { UsersTokensRepository } from '@modules/account/infra/typeorm/repositories/UsersTokensRepository';
 import { AppError } from '@shared/errors/AppError';
 
 interface IPayload {
@@ -14,6 +16,8 @@ export async function ensureAuthenticated(
 ) {
   const authHeader = request.headers.authorization;
 
+  const usersTokensRepository = new UsersTokensRepository();
+
   if (!authHeader) {
     throw new AppError('Token missing', 401);
   }
@@ -21,10 +25,13 @@ export async function ensureAuthenticated(
   const [, token] = authHeader.split(' ');
 
   try {
-    const { sub: user_id } = verify(token, 'ceede8b535cec4194415335ea5a5b854') as IPayload;
+    const { sub: user_id } = verify(token, auth.secret_refresh_token) as IPayload;
 
-    const userRepository = new UsersRepository();
-    const user = await userRepository.findById(user_id);
+    const user = await usersTokensRepository.findByUserIdAndRefreshToken(
+      user_id,
+      token,
+    );
+
     if (!user) {
       throw new AppError('User does not exist', 401);
     }
