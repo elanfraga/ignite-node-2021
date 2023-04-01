9e0965e1199a7653bc9f247d0b2fd0a601efe0ad
Elan Fraga <elanfraga@gmail.com>
2023-04-01 16:46:06 -0300

    Caso de uso de reset de senha

--- rentx-locadora/src/shared/infra/http/routes/password.routes.ts
+++ rentx-locadora/src/shared/infra/http/routes/password.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 
+import { ResetPasswordUserController } from '@modules/account/useCases/resetPasswordUser/ResetPasswordUserController';
 import { SendForgotPasswordMailController } from '@modules/account/useCases/sendForgotPasswordMail/SendForgotPasswordMailController';
 
 const passwordRoutes = Router();
 
 const sendforgotPasswordMailController = new SendForgotPasswordMailController();
+const resetPasswordUserController = new ResetPasswordUserController();
 
 passwordRoutes.post('/forgot', sendforgotPasswordMailController.handle);
+passwordRoutes.post('/reset', resetPasswordUserController.handle);
 
 export { passwordRoutes };
